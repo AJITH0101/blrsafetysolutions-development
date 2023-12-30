5bfdd3481e153fed4c9f853ae3efc7ab432c8d6c
My Javascript <ajith.silver@gmail.com>
2023-12-30 22:39:05 +0530

    Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Home from './Home'
 
 const App = () => {
   return (
     <>
-      <div className='text-3xl text-green-800'>App</div>
+      <Home />
     </>
     
   )
