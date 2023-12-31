ae300b779377051f592b54cbc6d297d563bf9e1b
My Javascript <ajith.silver@gmail.com>
2023-12-31 13:26:08 +0530

    Blr safety Solutions dev1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
-import Home from './Home'
+import './style.css'
 
 const App = () => {
   return (
     <>
-      <Navbar />
-    </>
-    
+      <Navbar />      
+    </>    
   )
 }
 
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
 
+
 const Navbar = () => {
   return (
-
     <>
-    <div>Home</div>
-    <div>Products</div>
-    <div>Career</div>
-    <div>About Us</div>
-    <div>Contact Us</div>
-    </>
-   
+    <div className='lg:w-full md:w-full lg:h-24 md:h-24 bg-white  text-black flex flex-row justify-between'>
+
+    <div className='w-64'>
+    <img src={process.env.PUBLIC_URL + '/logo.png'} alt="Logo" />
+    </div>  
+
+    
+
+
+
+    <div className='lg:flex lg:flex-row md:flex md:flex-row lg:pt-6 md:pt-6 text-lg lg:pr-8'>
+    <div className='lg:px-5 md:px-5'>Home</div>
+    <div className='lg:px-5 md:px-5'>Products</div>
+    <div className='lg:px-5 md:px-5'>Career</div>
+    <div className='lg:px-5 md:px-5'>About Us</div>
+    <div className='lg:px-5 md:px-5'>Contact Us</div>
+    </div>
+    </div>
+    </>   
   )
 }
 
