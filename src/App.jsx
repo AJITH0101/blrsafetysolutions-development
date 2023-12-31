21b6cb991c035021dbe5041dba1742b72ce6bf37
My Javascript <ajith.silver@gmail.com>
2023-12-31 09:33:41 +0530

    Blr Safety Solutions 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from './Navbar'
 import Home from './Home'
 
 const App = () => {
   return (
     <>
-      <Home />
+      <Navbar />
     </>
     
   )
---
+++ src/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+
+    <>
+    <div>Home</div>
+    <div>Products</div>
+    <div>Career</div>
+    <div>About Us</div>
+    <div>Contact Us</div>
+    </>
+   
+  )
+}
+
+export default Navbar
